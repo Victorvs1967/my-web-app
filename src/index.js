e086c941ecf48fe44dff12ecd456b3c4abe609e4
Victor <victorsmirnov@me.com>
2020-12-03 21:10:21 +0200

    add fonts

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle'
+import 'bootstrap/dist/js/bootstrap.bundle';
+import './fontawesome/css/all.min.css'
 
 import './index.css';
 import App from './App';
